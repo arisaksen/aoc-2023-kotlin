c7496ce83db8521f662e0a8e05e76a05f4741b51
Arne Ruben Isaksen <arisaksen@gmail.com>
2023-12-06 01:28:59 +0100

    Day04 part2

--- src/day04/Day04Part2.kt
+++ src/day04/Day04Part2.kt
@@ -3,33 +3,8 @@ package day04
 import org.assertj.core.api.Assertions.assertThat
 import readInput
 
-fun scratchYourHeartOutAndCount(cardToScore: Map<String, Int>): Int {
-    val stackOfCards = mutableListOf<Pair<String, Int>>()
-    cardToScore.forEach { card ->
-        stackOfCards.add(card.key to card.value)
-    }
-    val stackOfCardsScratched = ArrayDeque<Pair<String, Int>>()
-
-    while (stackOfCards.size > 0) {
-        val nextCard = stackOfCards.removeFirst()
-        val nextCardNumber: Int = nextCard.first.filter { it.isDigit() }.toInt()
-        val nextCardScore: Int = nextCard.second
-        val size = stackOfCards.size
-        (1..nextCardScore).forEach { cardNumber ->
-            val newCard = "Card ${cardNumber+nextCardNumber}"
-            val newCardScore = cardToScore[newCard]
-            newCardScore?.let {
-                stackOfCards.add(newCard to newCardScore)
-            }
-        }
-        stackOfCardsScratched.add(nextCard)
-    }
-    return stackOfCardsScratched.size
-}
-
 
 fun main() {
-
 
     fun part2(input: List<String>): Int {
         val cards = input
@@ -42,12 +17,20 @@ fun main() {
             }
 
         val cardToWins: Map<String, Int> = cards.map { card ->
-            val winningNumber = (card.value.first intersect card.value.second)
+            val winningNumber = (card.value.first intersect card.value.second.toSet())
             val wins = winningNumber.size
             card.key to wins
         }.toMap()
 
-        return scratchYourHeartOutAndCount(cardToWins)
+        val scratchedCards: MutableList<Int> = MutableList(cardToWins.size) { 1 }
+        cardToWins.entries.mapIndexed { index, card ->
+            (1..card.value).forEach {
+                scratchedCards[index + it] += scratchedCards[index]
+            }
+        }
+
+        return scratchedCards.sum()
+
     }
 
 // test if implementation meets criteria from the description, like:
@@ -59,11 +42,9 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 """.trimIndent().split('\n')
-    //  readInput("Part1_test")
     assertThat(part2(testInput)).isEqualTo(30)
 
 // print the puzzle answer
     val puzzleInput = readInput("Part1")
-//    println(part2(puzzleInput))
-
+    println(part2(puzzleInput))
 }
